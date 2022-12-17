b1cb3fedd9907e92f8ca895395a9bd882b75e0e7
Megha <118997722+Meghachaudhary20@users.noreply.github.com>
2022-12-17 20:31:22 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdio.h>
+void main()
+{
+    int age;
+    printf("enter age!");
+    scanf("%d",&age);
+    if (age>=18){
+        printf("eligible for vote");
+    }
+    else{
+        printf("not eligible");
+        
+        
+    }
+    
+}
